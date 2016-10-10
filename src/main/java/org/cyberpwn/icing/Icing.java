12ac6c1a86732e0d2ea51734ba95edd9e56bf27e
Daniel Mills <danielmillst@gmail.com>
2016-10-10 02:41:13 -0400

    Initial

---
+++ src/main/java/org/cyberpwn/icing/Icing.java
@@ -0,0 +1,30 @@
+package org.cyberpwn.icing;
+
+import org.phantomapi.construct.Ghost;
+
+public class Icing extends Ghost
+{
+	@Override
+	public void preStart()
+	{
+		
+	}
+
+	@Override
+	public void onStart()
+	{
+		
+	}
+
+	@Override
+	public void onStop()
+	{
+		
+	}
+
+	@Override
+	public void postStop()
+	{
+		
+	}
+}
