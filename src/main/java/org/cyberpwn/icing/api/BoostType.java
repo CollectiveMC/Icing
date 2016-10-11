e315e268d7505507ac8e8cab95517d643ec1a83d
Daniel Mills <danielmillst@gmail.com>
2016-10-11 14:59:34 -0400

    Boost Type configurable

--- src/main/java/org/cyberpwn/icing/api/BoostType.java
+++ src/main/java/org/cyberpwn/icing/api/BoostType.java
@@ -1,17 +1,20 @@
 package org.cyberpwn.icing.api;
 
+import org.phantomapi.clust.DataCluster;
 import org.phantomapi.lang.GList;
 
 public abstract class BoostType implements BoosterMesh
 {
+	protected DataCluster cc;
 	protected final String name;
-	protected final int maxLevel;
+	protected int maxLevel;
 	protected final GList<BoosterInstance> instances;
 	
 	public BoostType(String name, int maxLevel)
 	{
 		this.name = name;
 		this.maxLevel = maxLevel;
+		cc = new DataCluster();
 		instances = new GList<BoosterInstance>();
 	}
 	
@@ -26,9 +29,6 @@ public abstract class BoostType implements BoosterMesh
 	{
 		return name;
 	}
-	
-	@Override
-	public abstract BoosterInstance createBooster(int level, String player);
 	
 	@Override
 	public GList<BoosterInstance> getActiveBoosts()
@@ -58,4 +58,31 @@ public abstract class BoostType implements BoosterMesh
 	{
 		instances.remove(instance);
 	}
+	
+	@Override
+	public String getCodeName()
+	{
+		return "clust-hack";
+	}
+	
+	@Override
+	public DataCluster getConfiguration()
+	{
+		return cc;
+	}
+	
+	@Override
+	public void onNewConfig()
+	{
+		cc.set("boosts." + name.toLowerCase().replaceAll(" ", "-") + ".max-level", maxLevel);
+	}
+	
+	@Override
+	public void onReadConfig()
+	{
+		maxLevel = cc.getInt("boosts." + name.toLowerCase().replaceAll(" ", "-") + ".max-level");
+	}
+	
+	@Override
+	public abstract BoosterInstance createBooster(int level, String player);
 }
