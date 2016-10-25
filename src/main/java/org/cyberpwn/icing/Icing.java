5e520fb37e279a639a0c20dc7757d808b45c2fef
Daniel Mills <danielmillst@gmail.com>
2016-10-24 21:47:36 -0400

    mod icing

--- src/main/java/org/cyberpwn/icing/Icing.java
+++ src/main/java/org/cyberpwn/icing/Icing.java
@@ -4,24 +4,27 @@ import org.phantomapi.construct.Ghost;
 
 public class Icing extends Ghost
 {
+	private BoostController bc;
+	
 	@Override
 	public void preStart()
 	{
-		
+		bc = new BoostController(this);
+		register(bc);
 	}
-
+	
 	@Override
 	public void onStart()
 	{
 		
 	}
-
+	
 	@Override
 	public void onStop()
 	{
 		
 	}
-
+	
 	@Override
 	public void postStop()
 	{
