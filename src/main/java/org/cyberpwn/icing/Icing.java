add812a6f65f8f26d112404cabf564abb8508189
Daniel Mills <danielmillst@gmail.com>
2016-10-30 19:50:38 -0400

    Icing instance and tag prov

--- src/main/java/org/cyberpwn/icing/Icing.java
+++ src/main/java/org/cyberpwn/icing/Icing.java
@@ -1,14 +1,18 @@
 package org.cyberpwn.icing;
 
 import org.phantomapi.construct.Ghost;
+import org.phantomapi.text.TagProvider;
+import org.phantomapi.util.C;
 
-public class Icing extends Ghost
+public class Icing extends Ghost implements TagProvider
 {
+	private static Icing inst;
 	private BoostController bc;
 	
 	@Override
 	public void preStart()
 	{
+		inst = this;
 		bc = new BoostController(this);
 		register(bc);
 	}
@@ -29,5 +33,22 @@ public class Icing extends Ghost
 	public void postStop()
 	{
 		
+	}
+	
+	@Override
+	public String getChatTag()
+	{
+		return C.DARK_GRAY + "[" + C.AQUA + "Icing" + C.DARK_GRAY + "]: " + C.GRAY;
+	}
+	
+	@Override
+	public String getChatTagHover()
+	{
+		return C.AQUA + "Icing under the cake!";
+	}
+	
+	public static Icing inst()
+	{
+		return inst;
 	}
 }
