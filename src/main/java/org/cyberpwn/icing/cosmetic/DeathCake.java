ee162e9657d46b463489a39177e441b0b641ea80
Daniel Mills <danielmillst@gmail.com>
2016-10-30 18:51:26 -0400

    death type

--- src/main/java/org/cyberpwn/icing/cosmetic/DeathCake.java
+++ src/main/java/org/cyberpwn/icing/cosmetic/DeathCake.java
@@ -4,9 +4,9 @@ import org.phantomapi.construct.Controllable;
 
 public abstract class DeathCake extends SlicedCake implements DeathSlice
 {
-	public DeathCake(Controllable parentController, String codeName, CakeType type, String name, String description)
+	public DeathCake(Controllable parentController, String codeName, String name, String description)
 	{
-		super(parentController, codeName, type, name, description);
+		super(parentController, codeName, CakeType.DEATH, name, description);
 	}
 	
 	@Override
