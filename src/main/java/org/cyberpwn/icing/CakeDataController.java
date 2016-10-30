f30ed85e6e6a973ff0290739a6a91d9329c6c498
Daniel Mills <danielmillst@gmail.com>
2016-10-30 19:43:34 -0400

    Cake data controller

---
+++ src/main/java/org/cyberpwn/icing/CakeDataController.java
@@ -0,0 +1,59 @@
+package org.cyberpwn.icing;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.cyberpwn.icing.cosmetic.CakePlayerData;
+import org.phantomapi.clust.DataController;
+import org.phantomapi.construct.Controllable;
+
+public class CakeDataController extends DataController<CakePlayerData, Player>
+{
+	public CakeDataController(Controllable parentController)
+	{
+		super(parentController);
+	}
+	
+	@Override
+	public CakePlayerData onLoad(Player p)
+	{
+		CakePlayerData cpd = new CakePlayerData(p);
+		loadMysql(cpd);
+		return cpd;
+	}
+	
+	@Override
+	public void onSave(Player p)
+	{
+		CakePlayerData cpd = cache.get(p);
+		saveMysql(cpd);
+	}
+	
+	@Override
+	public void onStart()
+	{
+		for(Player i : onlinePlayers())
+		{
+			onLoad(i);
+		}
+	}
+	
+	@Override
+	public void onStop()
+	{
+		saveAll();
+	}
+	
+	@EventHandler
+	public void on(PlayerJoinEvent e)
+	{
+		load(e.getPlayer());
+	}
+	
+	@EventHandler
+	public void on(PlayerQuitEvent e)
+	{
+		save(e.getPlayer());
+	}
+}
