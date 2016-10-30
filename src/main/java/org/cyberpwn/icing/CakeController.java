975309260df03ad2bb7297b7d364c7e81c6cec99
Daniel Mills <danielmillst@gmail.com>
2016-10-30 18:29:33 -0400

    Control the cake

---
+++ src/main/java/org/cyberpwn/icing/CakeController.java
@@ -0,0 +1,24 @@
+package org.cyberpwn.icing;
+
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Controller;
+
+public class CakeController extends Controller
+{
+	public CakeController(Controllable parentController)
+	{
+		super(parentController);
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+}
