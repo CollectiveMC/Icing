14bea857d138229b18b0214d3310fb3feb33e9b6
Daniel Mills <danielmillst@gmail.com>
2016-10-30 19:43:08 -0400

    Cake type attributes

--- src/main/java/org/cyberpwn/icing/cosmetic/CakeType.java
+++ src/main/java/org/cyberpwn/icing/cosmetic/CakeType.java
@@ -1,10 +1,40 @@
 package org.cyberpwn.icing.cosmetic;
 
+import org.bukkit.Material;
+import org.phantomapi.util.C;
+import org.phantomapi.world.MaterialBlock;
+
 public enum CakeType
 {
-	PLAYER,
-	ARROW,
-	KILL,
-	DEATH,
-	TELEPORT,
+	PLAYER(C.AQUA + "Player FX", C.AQUA + "Ambient effects played near you.", new MaterialBlock(Material.ENCHANTED_BOOK)),
+	ARROW(C.BLUE + "Arrow FX", C.BLACK + "Arrow effects played on the arrow.", new MaterialBlock(Material.ARROW)),
+	KILL(C.GREEN + "Kill FX", C.GREEN + "Effects played when you kill someone.", new MaterialBlock(Material.IRON_SWORD)),
+	DEATH(C.YELLOW + "Death FX", C.YELLOW + "Effects played when you are killed.", new MaterialBlock(Material.GOLD_CHESTPLATE)),
+	TELEPORT(C.GOLD + "Teleport FX", C.GOLD + "Effects played when you teleport.", new MaterialBlock(Material.EYE_OF_ENDER));
+	
+	private String name;
+	private String description;
+	private MaterialBlock mb;
+	
+	private CakeType(String name, String description, MaterialBlock mb)
+	{
+		this.name = name;
+		this.description = description;
+		this.mb = mb;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public MaterialBlock getMb()
+	{
+		return mb;
+	}
 }
