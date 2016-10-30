902869ced0e946fad150db5f431d5a5ee68a26e4
Daniel Mills <danielmillst@gmail.com>
2016-10-30 19:43:02 -0400

    Update DEATH CAKE

--- src/main/java/org/cyberpwn/icing/cosmetic/DeathCake.java
+++ src/main/java/org/cyberpwn/icing/cosmetic/DeathCake.java
@@ -1,12 +1,13 @@
 package org.cyberpwn.icing.cosmetic;
 
 import org.phantomapi.construct.Controllable;
+import org.phantomapi.world.MaterialBlock;
 
 public abstract class DeathCake extends SlicedCake implements DeathSlice
 {
-	public DeathCake(Controllable parentController, String codeName, String name, String description)
+	public DeathCake(Controllable parentController, String codeName, String name, String description, MaterialBlock mb)
 	{
-		super(parentController, codeName, CakeType.DEATH, name, description);
+		super(parentController, codeName, CakeType.DEATH, name, description, mb);
 	}
 	
 	@Override
