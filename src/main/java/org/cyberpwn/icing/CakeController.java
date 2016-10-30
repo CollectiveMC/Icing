3dfac45681ab6b562c6f2bc29b93d92cdc40dabd
Daniel Mills <danielmillst@gmail.com>
2016-10-30 18:46:11 -0400

    Process the cake

--- src/main/java/org/cyberpwn/icing/CakeController.java
+++ src/main/java/org/cyberpwn/icing/CakeController.java
@@ -1,19 +1,41 @@
 package org.cyberpwn.icing;
 
+import org.cyberpwn.icing.cosmetic.Cake;
+import org.cyberpwn.icing.cosmetic.CakeType;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
+import org.phantomapi.lang.GList;
+import org.phantomapi.lang.GMap;
 
 public class CakeController extends Controller
 {
+	private GMap<CakeType, GList<Cake>> cakes;
+	
 	public CakeController(Controllable parentController)
 	{
 		super(parentController);
+		
+		cakes = new GMap<CakeType, GList<Cake>>();
 	}
 	
 	@Override
 	public void onStart()
 	{
-		
+		for(Controllable i : getControllers())
+		{
+			if(i instanceof Cake)
+			{
+				Cake cake = (Cake) i;
+				CakeType cakeType = cake.getType();
+				
+				if(!cakes.containsKey(cakeType))
+				{
+					cakes.put(cakeType, new GList<Cake>());
+				}
+				
+				cakes.get(cakeType).add(cake);
+			}
+		}
 	}
 	
 	@Override
