2438a2701a4da2d3d7c63b5ea1218396a3465b56
Daniel Mills <danielmillst@gmail.com>
2016-10-30 20:52:16 -0400

    arrow none

---
+++ src/main/java/org/cyberpwn/icing/cakes/ArrowNone.java
@@ -0,0 +1,41 @@
+package org.cyberpwn.icing.cakes;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.cyberpwn.icing.cosmetic.ArrowCake;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.world.MaterialBlock;
+
+public class ArrowNone extends ArrowCake
+{
+	public ArrowNone(Controllable parentController)
+	{
+		super(parentController, "arrow-none", "&dNo Effect", "&5No Effect", new MaterialBlock(Material.BARRIER));
+	}
+	
+	@Override
+	public void onAmbient(Arrow a, Player shooter)
+	{
+		
+	}
+	
+	@Override
+	public void onHitPlayer(Arrow a, Player shooter, Player hit)
+	{
+		
+	}
+	
+	@Override
+	public void onHit(Arrow a, Player shooter, Location land)
+	{
+		
+	}
+	
+	@Override
+	public void onShoot(Arrow a, Player shooter)
+	{
+		
+	}
+}
