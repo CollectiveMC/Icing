93451fea18d8881c9e56923ebbe2904547b97a6d
Daniel Mills <danielmillst@gmail.com>
2016-10-30 21:58:09 -0400

    Arrow slice tweaks

--- src/main/java/org/cyberpwn/icing/cosmetic/ArrowSlice.java
+++ src/main/java/org/cyberpwn/icing/cosmetic/ArrowSlice.java
@@ -2,15 +2,16 @@ package org.cyberpwn.icing.cosmetic;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 public interface ArrowSlice
 {
 	public void onAmbient(Arrow a, Player shooter);
 	
-	public void onHitPlayer(Arrow a, Player shooter, Player hit);
-	
 	public void onHit(Arrow a, Player shooter, Location land);
 	
 	public void onShoot(Arrow a, Player shooter);
+	
+	public void onHitEntity(Arrow a, Player p, LivingEntity e);
 }
