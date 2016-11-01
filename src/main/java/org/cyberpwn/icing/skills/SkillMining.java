14406a47bd5531822757f48bd24d2ef568d8acf1
Daniel Mills <danielmillst@gmail.com>
2016-11-01 10:39:46 -0400

    Skill mining

--- src/main/java/org/cyberpwn/icing/skills/SkillMining.java
+++ src/main/java/org/cyberpwn/icing/skills/SkillMining.java
@@ -1,13 +1,20 @@
 package org.cyberpwn.icing.skills;
 
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.cyberpwn.icing.skill.BasicSkill;
+import org.cyberpwn.icing.xp.XPReason;
 import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Ticked;
+import org.phantomapi.event.FalseBlockBreakEvent;
 
+@Ticked(50)
 public class SkillMining extends BasicSkill
 {
 	public SkillMining(Controllable parentController)
 	{
-		super(parentController, "miner");
+		super(parentController, "miner", XPReason.MINING);
 	}
 	
 	@Override
@@ -17,8 +24,60 @@ public class SkillMining extends BasicSkill
 	}
 	
 	@Override
+	public void onTick()
+	{
+		popRewardMap();
+	}
+	
+	@Override
 	public void onStop()
 	{
 		
+	}
+	
+	@EventHandler
+	public void on(FalseBlockBreakEvent e)
+	{
+		on(new BlockBreakEvent(e.getBlock(), e.getPlayer()));
+	}
+	
+	@EventHandler
+	public void on(BlockBreakEvent e)
+	{
+		if(e.getBlock().getType().toString().endsWith("_ORE") && e.getPlayer().getItemInHand() != null && e.getPlayer().getItemInHand().getType().toString().endsWith("_PICKAXE"))
+		{
+			if(e.getPlayer().getItemInHand().containsEnchantment(Enchantment.SILK_TOUCH))
+			{
+				return;
+			}
+			
+			int xp = 0;
+			
+			switch(e.getBlock().getType())
+			{
+				case DIAMOND_ORE:
+					xp = 48;
+					break;
+				case REDSTONE_ORE:
+					xp = 12;
+					break;
+				case LAPIS_ORE:
+					xp = 36;
+					break;
+				case COAL_ORE:
+					xp = 4;
+					break;
+				case EMERALD_ORE:
+					xp = 72;
+					break;
+				default:
+					break;
+			}
+			
+			if(xp > 0)
+			{
+				addReward(e.getPlayer(), xp);
+			}
+		}
 	}
 }
