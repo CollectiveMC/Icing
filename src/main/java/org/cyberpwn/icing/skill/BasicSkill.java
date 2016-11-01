938ca9d87f3d010371f43410303abfd5b2416bf3
Daniel Mills <danielmillst@gmail.com>
2016-11-01 06:55:46 -0400

    Basic Skill

--- src/main/java/org/cyberpwn/icing/skill/BasicSkill.java
+++ src/main/java/org/cyberpwn/icing/skill/BasicSkill.java
@@ -1,6 +1,8 @@
 package org.cyberpwn.icing.skill;
 
 import org.bukkit.entity.Player;
+import org.cyberpwn.icing.Icing;
+import org.cyberpwn.icing.xp.XP;
 import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.construct.Controllable;
 
@@ -9,24 +11,6 @@ public class BasicSkill extends ConfigurableController implements Skill
 	public BasicSkill(Controllable parentController, String codeName)
 	{
 		super(parentController, codeName);
-	}
-	
-	@Override
-	public long getSkill(Player p)
-	{
-		return;
-	}
-	
-	@Override
-	public long getXp(Player p)
-	{
-		return 0;
-	}
-	
-	@Override
-	public void addXp(Player p, long amt)
-	{
-		
 	}
 	
 	@Override
@@ -39,5 +23,35 @@ public class BasicSkill extends ConfigurableController implements Skill
 	public void onStop()
 	{
 		
+	}
+	
+	@Override
+	public long getXp(Player p)
+	{
+		return Icing.inst().getSk().getSkillDataController().get(p).getSkill(getCodeName());
+	}
+	
+	@Override
+	public void addXp(Player p, long amt)
+	{
+		Icing.inst().getSk().getSkillDataController().get(p).addSkill(getCodeName(), amt);
+	}
+	
+	@Override
+	public long getLevel(Player p)
+	{
+		return XP.getLevelForXp(getXp(p));
+	}
+	
+	@Override
+	public double getProgress(Player p)
+	{
+		return XP.percentToNextLevel(getXp(p));
+	}
+	
+	@Override
+	public long getXpLeft(Player p)
+	{
+		return (long) XP.xpToNextLevel(getXp(p));
 	}
 }
