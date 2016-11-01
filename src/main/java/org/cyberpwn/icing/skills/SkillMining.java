2a57b6182ac5b585c41fcc53920b504f9f17e02f
Daniel Mills <danielmillst@gmail.com>
2016-11-01 07:00:53 -0400

    Skill miner

---
+++ src/main/java/org/cyberpwn/icing/skills/SkillMining.java
@@ -0,0 +1,24 @@
+package org.cyberpwn.icing.skills;
+
+import org.cyberpwn.icing.skill.BasicSkill;
+import org.phantomapi.construct.Controllable;
+
+public class SkillMining extends BasicSkill
+{
+	public SkillMining(Controllable parentController)
+	{
+		super(parentController, "miner");
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+}
