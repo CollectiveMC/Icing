f609690d1955164a4a3c8a8093b34d8b17184d58
Daniel Mills <danielmillst@gmail.com>
2016-11-01 04:25:54 -0400

    XP utils

---
+++ src/main/java/org/cyberpwn/icing/xp/XP.java
@@ -0,0 +1,39 @@
+package org.cyberpwn.icing.xp;
+
+import org.bukkit.entity.Player;
+import org.cyberpwn.icing.Icing;
+
+public class XP
+{
+	public static void giveXp(Player player, long xp, XPReason reason)
+	{
+		XPEvent e = new XPEvent(player, xp, reason);
+		Icing.inst().callEvent(e);
+		
+		if(!e.isCancelled())
+		{
+			XPPlayer xpp = Icing.inst().getXp().getXpDataController().get(player);
+			xpp.setXp(e.getXp());
+		}
+	}
+	
+	public static long getXp(Player player)
+	{
+		return Icing.inst().getXp().getXpDataController().get(player).getXp();
+	}
+	
+	public static void setXp(Player player, long xp)
+	{
+		Icing.inst().getXp().getXpDataController().get(player).setXp(xp);
+	}
+	
+	public static double getBoost(Player player)
+	{
+		return Icing.inst().getXp().getXpDataController().get(player).getBoost();
+	}
+	
+	public static void setBoost(Player player, double boost)
+	{
+		Icing.inst().getXp().getXpDataController().get(player).setBoost(boost);
+	}
+}
