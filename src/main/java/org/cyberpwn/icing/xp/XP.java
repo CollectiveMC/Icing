bcc0a0bfbe31b4457f6b3259fa5d02304a8953d2
Daniel Mills <danielmillst@gmail.com>
2016-11-01 05:33:45 -0400

    XP Math

--- src/main/java/org/cyberpwn/icing/xp/XP.java
+++ src/main/java/org/cyberpwn/icing/xp/XP.java
@@ -2,6 +2,8 @@ package org.cyberpwn.icing.xp;
 
 import org.bukkit.entity.Player;
 import org.cyberpwn.icing.Icing;
+import org.phantomapi.util.D;
+import org.phantomapi.util.F;
 
 public class XP
 {
@@ -34,5 +36,50 @@ public class XP
 	public static void setBoost(Player player, double boost)
 	{
 		Icing.inst().getXp().getXpDataController().get(player).setBoost(boost);
+	}
+	
+	public static double percentToNextLevel(long xp)
+	{
+		double level = getLevelForXp(xp);
+		double axp = getXpForLevel((long) level);
+		double bxp = getXpForLevel((long) (level + 1));
+		double cxp = xp - axp;
+		double dxp = bxp - axp;
+		return cxp / dxp;
+	}
+	
+	public static double xpToNextLevel(long xp)
+	{
+		double level = getLevelForXp(xp);
+		double axp = getXpForLevel((long) level);
+		double bxp = getXpForLevel((long) (level + 1));
+		double cxp = xp - axp;
+		double dxp = bxp - axp;
+		
+		return dxp - cxp;
+	}
+	
+	public static long getXpForLevel(long level)
+	{
+		return (long) Math.pow((double) level, 3);
+	}
+	
+	public static long getLevelForXp(long xp)
+	{
+		return (long) Math.cbrt((double) xp);
+	}
+	
+	public static void printLeveling(int maxLevel)
+	{
+		long vxp = getXpForLevel(1);
+		long vle = 1;
+		D d = new D("X");
+		
+		while(vle < maxLevel)
+		{
+			vle = getLevelForXp(vxp);
+			vxp = getXpForLevel(vle + 1);
+			d.s("Level " + F.f(vle) + " needs " + F.f(vxp - getXpForLevel(vle)) + " XP, Total of " + F.f(getXpForLevel(vle)) + " XP");
+		}
 	}
 }
