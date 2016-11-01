6bad5661c84bef05067ca4f233d5ca5a25290cd9
Daniel Mills <danielmillst@gmail.com>
2016-11-01 04:09:33 -0400

    Base xp controller

---
+++ src/main/java/org/cyberpwn/icing/XPController.java
@@ -0,0 +1,25 @@
+package org.cyberpwn.icing;
+
+import org.phantomapi.clust.ConfigurableController;
+import org.phantomapi.construct.Controllable;
+
+public class XPController extends ConfigurableController
+{
+	
+	public XPController(Controllable parentController)
+	{
+		super(parentController, "xp");
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+}
