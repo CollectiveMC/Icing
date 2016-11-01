e05d0cd9d90014e46b8568bde4ef9034bb5a8e07
Daniel Mills <danielmillst@gmail.com>
2016-11-01 04:25:27 -0400

    XPData registry

--- src/main/java/org/cyberpwn/icing/XPController.java
+++ src/main/java/org/cyberpwn/icing/XPController.java
@@ -1,14 +1,20 @@
 package org.cyberpwn.icing;
 
+import org.cyberpwn.icing.xp.XPDataController;
 import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.construct.Controllable;
 
 public class XPController extends ConfigurableController
 {
+	private XPDataController xpDataController;
 	
 	public XPController(Controllable parentController)
 	{
 		super(parentController, "xp");
+		
+		xpDataController = new XPDataController(this);
+		
+		register(xpDataController);
 	}
 	
 	@Override
@@ -21,5 +27,10 @@ public class XPController extends ConfigurableController
 	public void onStop()
 	{
 		
+	}
+	
+	public XPDataController getXpDataController()
+	{
+		return xpDataController;
 	}
 }
