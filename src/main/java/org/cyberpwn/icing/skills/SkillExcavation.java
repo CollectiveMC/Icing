07828d45c3b868831cdd69e3fd61f3ce2f02ee68
Daniel Mills <danielmillst@gmail.com>
2016-11-02 02:55:39 -0400

    Skill excavation

---
+++ src/main/java/org/cyberpwn/icing/skills/SkillExcavation.java
@@ -0,0 +1,77 @@
+package org.cyberpwn.icing.skills;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.cyberpwn.icing.skill.BasicSkill;
+import org.cyberpwn.icing.xp.XPReason;
+import org.phantomapi.clust.Keyed;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Ticked;
+import org.phantomapi.event.FalseBlockBreakEvent;
+import org.phantomapi.lang.GMap;
+import org.phantomapi.world.MaterialBlock;
+
+@Ticked(40)
+public class SkillExcavation extends BasicSkill
+{
+	private GMap<Player, Integer> exc;
+	
+	@Keyed("blocks-per-xp")
+	public int bpxp = 6;
+	
+	public SkillExcavation(Controllable parentController)
+	{
+		super(parentController, "excavation", XPReason.EXCAVATION);
+		
+		exc = new GMap<Player, Integer>();
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onTick()
+	{
+		popRewardMap();
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	@EventHandler
+	public void on(FalseBlockBreakEvent e)
+	{
+		on(new BlockBreakEvent(e.getBlock(), e.getPlayer()));
+	}
+	
+	@EventHandler
+	public void on(BlockBreakEvent e)
+	{
+		if(!exc.containsKey(e.getPlayer()))
+		{
+			exc.put(e.getPlayer(), 0);
+		}
+		
+		exc.put(e.getPlayer(), exc.get(e.getPlayer()) + 1);
+		
+		if(exc.get(e.getPlayer()) > 150 + (Math.random() * 50))
+		{
+			addReward(e.getPlayer(), exc.get(e.getPlayer()) / bpxp);
+			exc.remove(e.getPlayer());
+		}
+	}
+	
+	@Override
+	public MaterialBlock getSkillMaterial()
+	{
+		return new MaterialBlock(Material.IRON_SPADE);
+	}
+}
