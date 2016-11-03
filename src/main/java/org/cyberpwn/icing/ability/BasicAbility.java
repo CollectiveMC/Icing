5de7fa5fe79f8c5a0b04e3715368f2154360d32c
Daniel Mills <danielmillst@gmail.com>
2016-11-03 05:35:44 -0400

    Impl base ability

--- src/main/java/org/cyberpwn/icing/ability/BasicAbility.java
+++ src/main/java/org/cyberpwn/icing/ability/BasicAbility.java
@@ -10,11 +10,15 @@ import org.phantomapi.clust.ConfigurableController;
 import org.phantomapi.clust.Keyed;
 import org.phantomapi.construct.Controller;
 
-public class BasicAbility extends ConfigurableController implements Ability
+public abstract class BasicAbility extends ConfigurableController implements Ability
 {
 	@Comment("The level a player's skill and level must be at to unlock this ability.")
 	@Keyed("ability.level")
 	public int level = 1;
+	
+	@Comment("The max level this ability can have (absolute max is 64)")
+	@Keyed("ability.max")
+	public int maxLevel = 64;
 	
 	@Comment("The level increase from the initial or last upgrade to upgrade again.")
 	@Keyed("ability.step")
@@ -33,26 +37,31 @@ public class BasicAbility extends ConfigurableController implements Ability
 		super((Controller) parent, codeName);
 	}
 	
+	@Override
 	public boolean isUnlocked(Player p)
 	{
 		return Icing.getInst().getSk().getAbilityDataController().get(p).getUnlockedAbilities().contains(getCodeName());
 	}
 	
+	@Override
 	public long getLevel(Player p)
 	{
 		return Icing.getInst().getSk().getAbilityDataController().get(p).getAbilityLevel(getCodeName());
 	}
 	
+	@Override
 	public void addLevel(Player p)
 	{
 		Icing.getInst().getSk().getAbilityDataController().get(p).addAbilityLevel(getCodeName(), 1);
 	}
 	
+	@Override
 	public Skill getSkill()
 	{
 		return (Skill) parentController;
 	}
 	
+	@Override
 	public boolean canUpgradeOrUnlock(Player p)
 	{
 		if(getSkill().getLevel(p) >= getMinimumUpgradeLevel(p) && XP.getLevelForXp(XP.getXp(p)) >= getMinimumUpgradeLevel(p))
@@ -63,6 +72,7 @@ public class BasicAbility extends ConfigurableController implements Ability
 		return false;
 	}
 	
+	@Override
 	public long getMinimumUpgradeLevel(Player p)
 	{
 		long alevel = getLevel(p);
@@ -75,45 +85,45 @@ public class BasicAbility extends ConfigurableController implements Ability
 		return level + ((alevel + 1) * levelStep);
 	}
 	
+	@Override
 	public int getLevel()
 	{
 		return level;
 	}
 	
+	@Override
 	public int getLevelStep()
 	{
 		return levelStep;
 	}
 	
+	@Override
 	public int getUnlockCost()
 	{
 		return unlockCost;
 	}
 	
+	@Override
 	public int getUpgradeCost()
 	{
 		return upgradeCost;
 	}
 	
+	@Override
 	public String name()
 	{
 		return getCodeName();
 	}
 	
+	@Override
 	public String fancyName()
 	{
 		return StringUtils.capitalize(getCodeName());
 	}
 	
 	@Override
-	public void onStart()
+	public int getMaxLevel()
 	{
-		
-	}
-	
-	@Override
-	public void onStop()
-	{
-		
+		return maxLevel;
 	}
 }
