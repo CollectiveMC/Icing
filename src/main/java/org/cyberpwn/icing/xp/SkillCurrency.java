7023896f676fda8fe0e93713457e3860cb863210
Daniel Mills <danielmillst@gmail.com>
2016-11-03 03:17:11 -0400

    Skill currency adaptation

--- src/main/java/org/cyberpwn/icing/xp/SkillCurrency.java
+++ src/main/java/org/cyberpwn/icing/xp/SkillCurrency.java
@@ -1,26 +1,34 @@
 package org.cyberpwn.icing.xp;
 
 import org.bukkit.entity.Player;
+import org.cyberpwn.icing.Icing;
 import org.phantomapi.currency.Currency;
 
 public class SkillCurrency implements Currency
 {
+	private String sk;
+	
+	public SkillCurrency(String codeName)
+	{
+		sk = codeName;
+	}
+	
 	@Override
 	public double get(Player p)
 	{
-		return XP.getSkill(p);
+		return Icing.getInst().getSk().getSkillDataController().get(p).getSkillPoints(sk);
 	}
 	
 	@Override
 	public void give(Player p, double amt)
 	{
-		XP.addSkill(p, (long) amt);
+		Icing.getInst().getSk().getSkillDataController().get(p).addSkillPoints(sk, (long) amt);
 	}
 	
 	@Override
 	public void take(Player p, double amt)
 	{
-		XP.takeSkill(p, (long) amt);
+		Icing.getInst().getSk().getSkillDataController().get(p).takeSkillPoints(sk, (long) amt);
 	}
 	
 	@Override
