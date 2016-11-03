428f6d25a79b9358e3781749d0c447be77f5bfd1
Daniel Mills <danielmillst@gmail.com>
2016-11-03 03:16:15 -0400

    Skilled player for buffers and shards

--- src/main/java/org/cyberpwn/icing/skill/SkilledPlayer.java
+++ src/main/java/org/cyberpwn/icing/skill/SkilledPlayer.java
@@ -51,4 +51,54 @@ public class SkilledPlayer extends ConfigurableObject
 		
 		return 0;
 	}
+	
+	public void setSkillPoints(String skill, long amt)
+	{
+		getConfiguration().set(skill + "-p", amt);
+	}
+	
+	public void addSkillPoints(String skill, long amt)
+	{
+		setSkillPoints(skill, amt + getSkillPoints(skill));
+	}
+	
+	public void takeSkillPoints(String skill, long amt)
+	{
+		setSkillPoints(skill, getSkillPoints(skill) - amt);
+	}
+	
+	public long getSkillPoints(String skill)
+	{
+		if(getConfiguration().contains(skill + "-p"))
+		{
+			return getConfiguration().getLong(skill + "-p");
+		}
+		
+		return 0;
+	}
+	
+	public void setSkillBuff(String skill, long amt)
+	{
+		getConfiguration().set(skill + "-b", amt);
+	}
+	
+	public void addSkillBuff(String skill, long amt)
+	{
+		setSkillBuff(skill, amt + getSkillBuff(skill));
+	}
+	
+	public void takeSkillBuff(String skill, long amt)
+	{
+		setSkillBuff(skill, getSkillBuff(skill) - amt);
+	}
+	
+	public long getSkillBuff(String skill)
+	{
+		if(getConfiguration().contains(skill + "-b"))
+		{
+			return getConfiguration().getLong(skill + "-b");
+		}
+		
+		return 0;
+	}
 }
