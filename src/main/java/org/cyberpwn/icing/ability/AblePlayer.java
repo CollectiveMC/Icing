7325f089acdad50186dfdb129ebd60934a9cf885
Daniel Mills <danielmillst@gmail.com>
2016-11-03 04:28:09 -0400

    Ability player

---
+++ src/main/java/org/cyberpwn/icing/ability/AblePlayer.java
@@ -0,0 +1,54 @@
+package org.cyberpwn.icing.ability;
+
+import org.bukkit.entity.Player;
+import org.phantomapi.clust.ConfigurableObject;
+import org.phantomapi.clust.Tabled;
+import org.phantomapi.lang.GList;
+
+@Tabled("ice_ability_player")
+public class AblePlayer extends ConfigurableObject
+{
+	private Player player;
+	
+	public AblePlayer(Player player)
+	{
+		super(player.getUniqueId().toString());
+	}
+	
+	public Player getPlayer()
+	{
+		return player;
+	}
+	
+	public GList<String> getUnlockedAbilities()
+	{
+		GList<String> abilities = new GList<String>();
+		
+		for(String i : getConfiguration().keys())
+		{
+			abilities.add(i);
+		}
+		
+		return abilities;
+	}
+	
+	public void setAbilityLevel(String ability, long level)
+	{
+		getConfiguration().set(ability, level);
+	}
+	
+	public void addSkill(String ability, long level)
+	{
+		setAbilityLevel(ability, level + getAbilityLevel(ability));
+	}
+	
+	public long getAbilityLevel(String ability)
+	{
+		if(getConfiguration().contains(ability))
+		{
+			return getConfiguration().getLong(ability);
+		}
+		
+		return 0;
+	}
+}
