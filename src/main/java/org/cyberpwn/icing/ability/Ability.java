6bf96215a539edf6a9793f9ea867cfe2363653c3
Daniel Mills <danielmillst@gmail.com>
2016-11-03 04:46:21 -0400

    Ability interface

--- src/main/java/org/cyberpwn/icing/ability/Ability.java
+++ src/main/java/org/cyberpwn/icing/ability/Ability.java
@@ -1,6 +1,31 @@
 package org.cyberpwn.icing.ability;
 
+import org.bukkit.entity.Player;
+import org.cyberpwn.icing.skill.Skill;
+
 public interface Ability
 {
+	public boolean isUnlocked(Player p);
 	
+	public long getLevel(Player p);
+	
+	public void addLevel(Player p);
+	
+	public Skill getSkill();
+	
+	public boolean canUpgradeOrUnlock(Player p);
+	
+	public long getMinimumUpgradeLevel(Player p);
+	
+	public int getLevel();
+	
+	public int getLevelStep();
+	
+	public int getUnlockCost();
+	
+	public int getUpgradeCost();
+	
+	public String name();
+	
+	public String fancyName();
 }
