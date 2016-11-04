a0761f5a234549efcd848e8341c61851df13855e
Daniel Mills <danielmillst@gmail.com>
2016-11-04 16:13:29 -0400

    Unarmed skill

---
+++ src/main/java/org/cyberpwn/icing/skills/SkillUnarmed.java
@@ -0,0 +1,71 @@
+package org.cyberpwn.icing.skills;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.cyberpwn.icing.abilities.UnarmedForce;
+import org.cyberpwn.icing.abilities.UnarmedShareCare;
+import org.cyberpwn.icing.skill.BasicSkill;
+import org.cyberpwn.icing.xp.XPReason;
+import org.phantomapi.clust.Keyed;
+import org.phantomapi.construct.Controllable;
+import org.phantomapi.construct.Ticked;
+import org.phantomapi.world.MaterialBlock;
+
+@Ticked(200)
+public class SkillUnarmed extends BasicSkill
+{
+	@Keyed("initial")
+	public int initial = 3;
+	
+	public SkillUnarmed(Controllable parentController)
+	{
+		super(parentController, "unarmed", XPReason.UNARMED);
+	}
+	
+	@Override
+	public void onStart()
+	{
+		
+	}
+	
+	@Override
+	public void onTick()
+	{
+		popRewardMap();
+	}
+	
+	@Override
+	public void onStop()
+	{
+		
+	}
+	
+	@EventHandler
+	public void on(EntityDamageByEntityEvent e)
+	{
+		if(e.getDamager() instanceof Player)
+		{
+			Player p = (Player) e.getDamager();
+			
+			if(p.getItemInHand() == null || p.getItemInHand().getType().equals(Material.AIR))
+			{
+				addReward(p, (int) (initial * e.getDamage()));
+			}
+		}
+	}
+	
+	@Override
+	public MaterialBlock getSkillMaterial()
+	{
+		return new MaterialBlock(Material.ROTTEN_FLESH);
+	}
+	
+	@Override
+	public void createControllers()
+	{
+		register(new UnarmedForce(this));
+		register(new UnarmedShareCare(this));
+	}
+}
