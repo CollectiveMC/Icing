25682e70714f403dbccf9da1de4e2e849f75dfc5
Daniel Mills <danielmillst@gmail.com>
2016-11-05 18:01:28 -0400

    Swords events

--- src/main/java/org/cyberpwn/icing/skills/SkillSwords.java
+++ src/main/java/org/cyberpwn/icing/skills/SkillSwords.java
@@ -3,6 +3,7 @@ package org.cyberpwn.icing.skills;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.cyberpwn.icing.skill.BasicSkill;
 import org.cyberpwn.icing.xp.XPReason;
@@ -40,9 +41,14 @@ public class SkillSwords extends BasicSkill
 		
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void on(EntityDamageByEntityEvent e)
 	{
+		if(e.isCancelled())
+		{
+			return;
+		}
+		
 		if(e.getDamager() instanceof Player)
 		{
 			Player p = (Player) e.getDamager();
