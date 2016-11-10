18e83a665ad1790c7627c14c6bf0ad800218e7f2
Daniel Mills <danielmillst@gmail.com>
2016-11-10 17:18:02 -0500

    Placeholders

--- src/main/java/org/cyberpwn/icing/PlaceholderController.java
+++ src/main/java/org/cyberpwn/icing/PlaceholderController.java
@@ -2,6 +2,7 @@ package org.cyberpwn.icing;
 
 import org.bukkit.entity.Player;
 import org.cyberpwn.icing.xp.XP;
+import org.phantomapi.lang.GTime;
 import org.phantomapi.placeholder.PlaceholderHook;
 import org.phantomapi.util.F;
 
@@ -23,6 +24,21 @@ public class PlaceholderController extends PlaceholderHook
 		if(q.equalsIgnoreCase("x_boost"))
 		{
 			return F.pc(XP.getBoost(p));
+		}
+		
+		if(q.equalsIgnoreCase("x_boost_amt"))
+		{
+			return F.pc(Icing.inst().getSk().getXpp(p).getBoosterAmount());
+		}
+		
+		if(q.equalsIgnoreCase("x_boost_time"))
+		{
+			return new GTime(Icing.inst().getSk().getXpp(p).getBoosterTicks() * 50).to();
+		}
+		
+		if(q.equalsIgnoreCase("x_discred"))
+		{
+			return F.pc(Icing.inst().getSk().getXpp(p).getDiscredit());
 		}
 		
 		return null;
