82b4f008dd66b870211ee1feb27e9d7948eba04e
Daniel Mills <danielmillst@gmail.com>
2016-11-30 00:20:56 -0500

    Reg shatter

--- src/main/java/org/cyberpwn/icing/skills/SkillAxes.java
+++ src/main/java/org/cyberpwn/icing/skills/SkillAxes.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.cyberpwn.icing.abilities.AxesShatter;
 import org.cyberpwn.icing.skill.BasicSkill;
 import org.cyberpwn.icing.xp.XP;
 import org.cyberpwn.icing.xp.XPReason;
@@ -83,6 +84,6 @@ public class SkillAxes extends BasicSkill
 	@Override
 	public void createControllers()
 	{
-		
+		register(new AxesShatter(this));
 	}
 }
